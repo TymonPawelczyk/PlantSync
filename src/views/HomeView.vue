c26fc7f4b0951251111ebe6c35b4b566c16ca0dd
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-05 18:53:31 +0200

    Added Profile button to HomePage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,4 @@
-<script lang="ts" setup>
-import ProfileView from './ProfileView.vue'
-</script>
+<script lang="ts" setup></script>
 <template>
   <h1 class="text-5xl font-bold text-center font-stretch-90">
     Welcome to PlantSync!
@@ -12,7 +10,23 @@ import ProfileView from './ProfileView.vue'
       Obecna pogoda i zadania na dzisiaj w formie listy oraz ostanio dodana roślina
     </h1>
   </div>
-  <ProfileView />
+  <RouterLink to="/profile" class="btn">
+    <font-awesome-icon icon="fa-solid fa-user" />
+  </RouterLink>
 </template>
 
-<style scoped></style>
+<style scoped>
+.btn {
+  display: grid;
+  place-items: center;
+  background-color: var(--color-stone-200);
+  color: black;
+  margin: 15%;
+  border-radius: 5px;
+  &:hover {
+    background-color: var(--color-stone-700);
+    color: white;
+    transition: 0.4s;
+  }
+}
+</style>
