dcf251b3e3d83a4e3d5f524a6475f2d34bf00fd0
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-05 04:23:08 +0200

    Routing file changed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,32 @@
+// src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import HomeView from '@/views/HomeView.vue'
+import AddPlantView from '@/views/AddPlantView.vue'
+import PlantDetailsView from '@/views/PlantDetailsView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/add',
+    name: 'AddPlant',
+    component: AddPlantView
+  },
+  {
+    path: '/plant',
+    name: 'PlantDetails',
+    component: PlantDetailsView,
+    props: true
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    }  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
