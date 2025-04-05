58ebf7cfff06025eaf1ba4ba7628b3d9095fa757
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-05 04:24:02 +0200

    Added AddPlantView

---
+++ src/views/AddPlantView.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div class="p-4">
+    <RouterLink to="/" class="btn" name="Home">< Home</RouterLink>
+    <h1 class="text-xl font-bold mb-2">Dodaj roślinę</h1>
+    <!-- formularz dodawania -->
+     <!-- Router ponizej podpiety pod każda rosline z listy -->
+    <RouterLink to="/plant" class="text-blue-600 underline">Zobacz szczegóły</RouterLink>
+  </div>
+</template>
+
+<style>
+.btn {
+    background-color: var(--color-stone-200);
+    color: black;
+    &:hover {
+      background-color: var(--color-stone-700);
+      color: white;
+
+    }
+  }
+</style>
