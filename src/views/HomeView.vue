3e87062a4d37e93c4bae6032b3cf8ac64a31313a
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-05 17:51:36 +0200

    Eslint styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 <template>
   <h1 class="text-5xl font-bold text-center font-stretch-90">
-    Welsome to PlantSync!
+    Welcome to PlantSync!
     <font-awesome-icon icon="fa-solid fa-seedling" />
   </h1>
   <div class="p-4">
     <RouterLink to="/add" class="drop-shadow">+ Dodaj roślinę</RouterLink>
-    <h1 class="text-2xl">Obecna pogoda i  zadania na dzisiaj w formie listy oraz ostanio dodana roślina</h1>
+    <h1 class="text-2xl">
+      Obecna pogoda i zadania na dzisiaj w formie listy oraz ostanio dodana roślina
+    </h1>
   </div>
-  
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
