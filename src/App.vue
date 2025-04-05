e13f8096e7b44e79100bc33d96b55b2e875d8cde
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-05 04:19:25 +0200

    Added routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import router from './router';
 import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <HomeView />
+  <router-view />
 </template>
 
 <style scoped>
