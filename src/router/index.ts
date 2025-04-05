8b9ffa5e97c360b606a3e49c3aab92f277286632
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-05 17:43:42 +0200

    Added routing to profile

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,29 +4,35 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AddPlantView from '@/views/AddPlantView.vue'
 import PlantDetailsView from '@/views/PlantDetailsView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/add',
     name: 'AddPlant',
-    component: AddPlantView
+    component: AddPlantView,
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileView,
   },
   {
     path: '/plant',
     name: 'PlantDetails',
     component: PlantDetailsView,
-    props: true
-  }
+    props: true,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
