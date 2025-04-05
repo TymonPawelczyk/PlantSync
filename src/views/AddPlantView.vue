d06df0beb60ead5cd3fc3cf544b9eaa87636a690
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2025-04-05 17:30:17 +0200

    Eslint comment fix

--- src/views/AddPlantView.vue
+++ src/views/AddPlantView.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
     <div class="p-4">
-    <RouterLink to="/" class="btn" name="Home">< Home</RouterLink>
+    <RouterLink to="/" class="btn" name="Home"> Home </RouterLink>
     <h1 class="text-xl font-bold mb-2">Dodaj roślinę</h1>
     <!-- formularz dodawania -->
      <!-- Router ponizej podpiety pod każda rosline z listy -->
